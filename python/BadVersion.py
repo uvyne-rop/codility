1f2bc5d4686d07f6d758812f5970bcff3fead66f
uvyne-rop <ropchiri7@gmail.com>
2024-07-25 12:12:48 +0300

    badversion

---
+++ python/BadVersion.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fBadVersion(self, n):
+
+        left = 1
+        right = n
+        while left < right:
+            mid = (left + right) //2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
